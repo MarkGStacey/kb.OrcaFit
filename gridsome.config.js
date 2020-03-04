9c966315b29f8cd16f93ca12be74719cdb374f3a
Mark <mark.stacey@aphelion.bi>
2020-03-04 10:44:24 +0000

    Algolia

--- gridsome.config.js
+++ gridsome.config.js
@@ -3,7 +3,33 @@
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
-
+const collections = [
+  {
+    query: `{
+      allBlogPost {
+        edges {
+          node {
+            id
+            title
+            slug
+            modified
+          }
+        }
+      }
+    }`,
+    transformer: ({ data }) => data.allBlogPost.edges.map(({ node }) => node),
+    indexName: 'kb.OrcaFit', // Algolia index name
+    itemFormatter: (item) => {
+      return {
+        objectID: item.id,
+        title: item.title,
+        slug: item.slug,
+        modified: String(item.modified)
+      }
+    }, // optional
+    matchFields: ['slug', 'modified'], // Array<String> required with PartialUpdates
+  },
+]
 module.exports = {
   siteName: 'Orca Fit Exercise Knowledge Base',
   siteDescription: 'A resource for building rehab as well as strength & conditioning programs',
@@ -13,7 +39,6 @@ module.exports = {
     Tag: '/tag/:id',
     Bodypart: '/bodypart/:id'
   },
-
   plugins: [
     {
       // Create posts from markdown files
@@ -33,6 +58,16 @@ module.exports = {
           }
         }
       }
+    },
+    {
+      use: `gridsome-plugin-algolia`,
+      options: {
+        appId: process.env.X8QGZL1SEH,
+        apiKey: process.env.da245f71d8e5c4b1b9beb99e113b44bf,
+        collections,
+        chunkSize: 10000, // default: 1000
+        enablePartialUpdates: true, // default: false
+      },
     }
   ],
 
