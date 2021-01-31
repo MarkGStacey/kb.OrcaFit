8a63b4d623b404ce025940deb5fbc4b286c3253f
Mark <mark.stacey@aphelion.bi>
2021-01-31 20:15:07 +0000

    active tab

--- src/components/General/Tabs.vue
+++ src/components/General/Tabs.vue
@@ -2,7 +2,7 @@
 <div class="post-card content-box p-0">
  <v-tabs>
       <v-tab><g-link :to="'/'">Posts</g-link></v-tab>
-      <v-tab><g-link :to="'Programs'">Programs</g-link></v-tab>
+      <v-tab href="programs"><g-link :to="'Programs'">Programs</g-link></v-tab>
     </v-tabs>
 </div>
 </template>
