a7f0778ea83c410310575e458da52dae43fd08c6
Mark <3523826+MarkGStacey@users.noreply.github.com>
2023-04-27 13:27:10 +0100

    New workout

--- src/components/General/WorkoutHeader.vue
+++ src/components/General/WorkoutHeader.vue
@@ -2,14 +2,19 @@
      <div class="content-box" style="padding:0px;">
        <v-toolbar>
       <SearchWorkouts/>
+      <NewWorkout class="p-2"/>
       </v-toolbar>
     </div>
 </template>
 <script>
+
+import NewWorkout from '~/components/Dialogs/newWorkout.vue'
 import SearchWorkouts from '~/components/General/SearchWorkouts.vue'
+
 export default {
   components: {
-    SearchWorkouts
+    SearchWorkouts,
+    NewWorkout
   }
 }
 </script>
